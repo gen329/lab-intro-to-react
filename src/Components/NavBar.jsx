6a3c57a64477ae7cc25705aefe4a00d4884e6470
Genesis Lara <genesislara@pursuit.org>
2023-07-25 22:10:44 -0400

    adds navbar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <NavBar>
+      <div>
+        <ul>
+          <li>
+            <a href="#">What is Pursuit?</a>
+          </li>
+          <li>
+            <a href="#">Create an account</a>
+          </li>
+          <li>
+            <a href="#">Sign in</a>
+          </li>
+        </ul>
+      </div>
+    </NavBar>
+  );
+}
+
+export default NavBar;
